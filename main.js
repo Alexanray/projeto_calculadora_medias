e7fc346246612229023c0ca91db01be1b802711e
Alexandre Jardim <alexanray@yahoo.com.br>
2023-03-02 19:58:04 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function adicionaLinha() {
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
     if (atividades.includes(inputNomeAtividade.value)) {
-    alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
     } else {
     atividades.push(inputNomeAtividade.value);
     notas.push(parseFloat(inputNotaAtividade.value));
@@ -32,23 +32,24 @@ function adicionaLinha() {
     linha += `<td>${inputNotaAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
     linha += '</tr>';
-    linhas += linha;
+
+        linhas += linha;
+    }
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
     }
-}
+
 function atualizaTabela(){
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
 }
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? 'Aprovado' : "Reprovado"; 
 
 function atualizaMediafinal() {
     const mediaFinal = calculaMediaFinal();
-    
-    console.log(media);
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado; 
 }
 
 function calculaMediaFinal(){
